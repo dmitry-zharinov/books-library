9f80b904d5240f3e14a10a7e477af782402f1afe
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-06-20 11:01:14 +0300

    Add redirect check

--- main.py
+++ main.py
@@ -5,15 +5,31 @@ import requests
 FOLDER_NAME = 'books'
 
 
-def download_book(url, filename):
-    response = requests.get(url)
+def download_book(url, payload, filename):
+    response = requests.get(url, payload)
     response.raise_for_status()
+    check_for_redirect(response)
     with open(Path(FOLDER_NAME) / filename, 'wb') as file:
         file.write(response.content)
 
-Path(FOLDER_NAME).mkdir(parents=True, exist_ok=True)
 
-for book_id in range(1, 10):
-    url = f'https://tululu.org/txt.php?id={book_id}'
-    download_book(url, f'id{book_id}.txt')
-        
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError('Книга не найдена')
+
+
+def main():
+    Path(FOLDER_NAME).mkdir(parents=True, exist_ok=True)
+
+    for book_id in range(1, 11):
+        payload = {
+            'id': book_id,
+        }
+        url = 'https://tululu.org/txt.php'
+        try:
+            download_book(url, payload, f'id{book_id}.txt')
+        except requests.exceptions.HTTPError as http_err:
+            print(http_err)
+
+
+main()
