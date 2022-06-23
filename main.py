0982d4c220dcdb69e2f7a0622f7751316353aa64
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-06-23 17:17:42 +0300

    Download book covers

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os
 from pathlib import Path
+from urllib.parse import urljoin, urlsplit
 
 import requests
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 
 FOLDER_NAME = 'books'
+IMG_FOLDER_NAME = 'images'
+HOST_NAME = 'https://tululu.org'
 
 
 def download_txt(url, payload, filename, folder):
@@ -57,11 +60,23 @@ def get_book_metadata(url):
     soup = BeautifulSoup(response.text, 'lxml')
 
     book_name = soup.find('td', class_='ow_px_td').find('h1').text.split('::')
+    img_url = soup.find('div', class_='bookimage').find('img')['src']
     book_info = {
         'title': book_name[0].strip(),
-        'author': book_name[1].strip()
+        'author': book_name[1].strip(),
+        'img': urljoin(HOST_NAME, img_url),
     }
     return book_info
+
+
+def download_image(url, folder):
+    """Функция для скачивания обложек"""
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    filename = urlsplit(url).path.split('/')[-1] 
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(Path(folder) / filename, 'wb') as file:
+        file.write(response.content)
 
 
 def main():
@@ -72,11 +87,16 @@ def main():
         }
         url = 'https://tululu.org/txt.php'
         try:
-            book_data = get_book_metadata(f'https://tululu.org/b{book_id}/')
+            book_data = get_book_metadata(
+                f'https://tululu.org/b{book_id}/')
+            print(book_data)
+            '''
             download_txt(url,
                          payload,
                          f'{book_id}. {book_data["title"]}',
                          FOLDER_NAME)
+            '''
+            download_image(book_data['img'], IMG_FOLDER_NAME)
         except requests.exceptions.HTTPError as http_err:
             print(http_err)
 
