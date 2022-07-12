39fc5296953f02313574aa036bb81564ff4749fe
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-07-12 19:55:17 +0300

    Get book links brom first 10 pages

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -6,11 +6,13 @@ from bs4 import BeautifulSoup
 
 def main():
     base_url = 'https://tululu.org/l55/'
-    response = requests.get(base_url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
-    for link in soup.find('body').find_all('table', class_='d_book'):
-        print(urljoin(base_url, link.find('a')['href']))
+    for page in range(1, 11):
+        page_url = f'{base_url}/{page}/'
+        response = requests.get(page_url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'lxml')
+        for link in soup.find('body').find_all('table', class_='d_book'):
+            print(urljoin(base_url, link.find('a')['href']))
 
 
 if __name__ == '__main__':
