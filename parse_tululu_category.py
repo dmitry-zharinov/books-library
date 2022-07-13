248894e243cd3b6d68ad9c88ab64b5f5ca6ad437
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-07-13 10:44:15 +0300

    Add arguments start_page and end_page

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import logging
 
@@ -6,13 +7,20 @@ from bs4 import BeautifulSoup
 
 from tululu_parser import download_book_with_image
 
-
 logger = logging.getLogger(__file__)
 
 
-def get_book_ids(genre_url, start_page, end_page):
-    book_ids = []
+def createParser():
+    """Создать парсер аргументов"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--start_page', type=int, default=1)
+    parser.add_argument('--end_page', type=int, default=2)
+    return parser
 
+
+def get_book_ids(genre_url: str, start_page: int, end_page: int):
+    """Получить id книг по заданным страницам"""
+    book_ids = []
     for page in range(start_page, end_page + 1):
         page_url = f'{genre_url}/{page}/'
         response = requests.get(page_url)
@@ -29,8 +37,14 @@ def main():
     logging.basicConfig(level=logging.ERROR)
     logger.setLevel(logging.DEBUG)
 
+    parser = createParser()
+    parser_params = parser.parse_args()
+
     sci_fi_url = 'https://tululu.org/l55/'
-    book_ids = get_book_ids(sci_fi_url, 1, 2)
+    book_ids = get_book_ids(
+        sci_fi_url,
+        parser_params.start_page,
+        parser_params.end_page)
     downloaded_books = [
         download_book_with_image(book_id) for book_id in book_ids]
     with open("books.json", "w", encoding="utf-8") as books_data_file:
