1d63719420f0e30bc8a90537e69d3c00b1d10819
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-07-13 12:45:30 +0300

    Fix errors

--- parse_tululu.py
+++ parse_tululu.py
@@ -1,17 +1,27 @@
 import argparse
 import logging
+from pathlib import Path
 
 from download_books import download_book_with_image
-
 
 logger = logging.getLogger(__file__)
 
 
 def createParser():
     """Создание парсера аргументов"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--start_id', type=int, default=1)
-    parser.add_argument('--end_id', type=int, default=10)
+    parser = argparse.ArgumentParser(
+        description='Скачивание книг по id'
+    )
+    parser.add_argument(
+        '--start_id',
+        help='С какой книги скачивать (по умолчанию - 1)',
+        type=int,
+        default=1)
+    parser.add_argument(
+        '--end_id',
+        help='По какую книгу скачивать (по умолчанию - 10)',
+        type=int,
+        default=10)
     return parser
 
 
@@ -20,10 +30,14 @@ def main():
     logger.setLevel(logging.DEBUG)
 
     parser = createParser()
-    parser_params = parser.parse_args()
+    args = parser.parse_args()
 
-    for book_id in range(parser_params.start_id, parser_params.end_id+1):
-        book_data = download_book_with_image(book_id)
+    for book_id in range(args.start_id, args.end_id+1):
+        book_data = download_book_with_image(
+            book_id=book_id,
+            dest_folder=Path.cwd(),
+            skip_imgs=False,
+            skip_txt=False)
         if book_data:
             logging.info(f'Заголовок: {book_data["title"]}')
             logging.info(book_data["genres"])
